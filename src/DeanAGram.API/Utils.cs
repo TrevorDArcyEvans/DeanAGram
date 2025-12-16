da239819eff161ddb140beec00a52754d3280b4c
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2025-12-16 13:35:12 +0000

    - initial skeleton

---
+++ src/DeanAGram.API/Utils.cs
@@ -0,0 +1,11 @@
+﻿namespace DeanAGram.API;
+
+public static class Utils
+{
+  public static string FastKey(this string str)
+  {
+    var unique = str.ToCharArray().Distinct().ToList();
+    unique.Sort();
+    return new string(unique.ToArray());
+  }
+}
---
+++ src/DeanAGram.Tests/Utils_Tests.cs
@@ -0,0 +1,18 @@
+﻿namespace DeanAGram.Tests;
+
+using DeanAGram.API;
+using Shouldly;
+
+[TestFixture]
+public sealed class Utils_Tests
+{
+  [TestCase("edcba", "abcde")]
+  [TestCase("edcbaedcba", "abcde")]
+  [TestCase("ababa", "ab")]
+  public void FastKey_returns_expected(string str, string expected)
+  {
+    var fkey = str.FastKey();
+     
+    fkey.ShouldBe(expected);
+  }
+}
