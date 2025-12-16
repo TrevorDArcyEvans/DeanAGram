7c29d4b3bbd120b97454f914b44cad2cf10ae581
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2025-12-16 15:46:50 +0000

    - implemented ContainsWord

--- src/DeanAGram.API/Utils.cs
+++ src/DeanAGram.API/Utils.cs
@@ -8,4 +8,40 @@ public static class Utils
     unique.Sort();
     return new string(unique.ToArray());
   }
+
+  public static bool ContainsWord(this string anagram, string word)
+  {
+    if (anagram.Length < word.Length)
+    {
+      return false;
+    }
+
+    var aChars = GetCharacterCounts(anagram);
+    var wChars = GetCharacterCounts(word);
+
+    foreach (var wkvp in wChars)
+    {
+      if (!aChars.ContainsKey(wkvp.Key))
+      {
+        // word char not in anagram
+        return false;
+      }
+
+      if (aChars[wkvp.Key] < wkvp.Value)
+      {
+        // not enough of this (word) char in anagram
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  private static IDictionary<char, int> GetCharacterCounts(string anagram)
+  {
+    return anagram
+      .GroupBy(c => c)
+      .Select(c => new { Char = c.Key, Count = c.Count() })
+      .ToDictionary(x => x.Char, x => x.Count);
+  }
 }
--- src/DeanAGram.Tests/Utils_Tests.cs
+++ src/DeanAGram.Tests/Utils_Tests.cs
@@ -12,7 +12,23 @@ public sealed class Utils_Tests
   public void FastKey_returns_expected(string str, string expected)
   {
     var fkey = str.FastKey();
-     
+
     fkey.ShouldBe(expected);
+  }
+
+  [TestCase("edcba", "abcde")]
+  [TestCase("edcbaedcba", "abcde")]
+  [TestCase("ababa", "ab")]
+  public void ContainsWord_contains_returns_true(string anagram, string word)
+  {
+    anagram.ContainsWord(word).ShouldBeTrue();
+  }
+
+  [TestCase("abc", "abcde")]
+  [TestCase("zxvnmjkl", "abcde")]
+  [TestCase("ab", "ababa")]
+  public void ContainsWord_not_contains_returns_false(string anagram, string word)
+  {
+    anagram.ContainsWord(word).ShouldBeFalse();
   }
 }
