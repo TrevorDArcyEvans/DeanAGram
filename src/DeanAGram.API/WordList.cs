6266c8ec5e6825cd792a4b3c1abb3c3a652ca388
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2025-12-16 15:23:02 +0000

    - implemented Contains - implemented GetCandidates

--- src/DeanAGram.API/WordList.cs
+++ src/DeanAGram.API/WordList.cs
@@ -1,6 +1,7 @@
 namespace DeanAGram.API;
 
 // [fast key] --> [words[]]
+// fast key = unique letters in a word
 public sealed class WordList : Dictionary<string, HashSet<string>>
 {
   public void Add(string word)
@@ -16,5 +17,35 @@ public sealed class WordList : Dictionary<string, HashSet<string>>
     {
       this[fkey].Add(word);
     }
+  }
+
+  public bool Contains(string word)
+  {
+    var fkey = word.FastKey();
+
+    return ContainsKey(fkey) && this[fkey].Contains(word);
+  }
+
+  public HashSet<string> GetCandidates(string anagram)
+  {
+    var retVal = new HashSet<string>();
+
+    // fast key = list of unique letters in anagram
+    var fkey = anagram.FastKey();
+
+    foreach (var key in Keys)
+    {
+      // each letter of word must be in anagram
+      var containsAll = key.All(ch => fkey.Contains(ch));
+      if (!containsAll)
+      {
+        continue;
+      }
+
+      var thisCandidates = this[key];
+      retVal.UnionWith(thisCandidates);
+    }
+
+    return retVal;
   }
 }
--- src/DeanAGram.Tests/WordList_Tests.cs
+++ src/DeanAGram.Tests/WordList_Tests.cs
@@ -27,4 +27,69 @@ public sealed class WordList_Tests
     words.Keys.Count.ShouldBe(1);
     words[words.Keys.Single()].Single().ShouldBe(word);
   }
+
+  [Test]
+  public void Contains_returns_false()
+  {
+    var words = new WordList { "edcba", "edcbaedcba" };
+
+    words.Contains(Guid.NewGuid().ToString()).ShouldBeFalse();
+  }
+
+  [Test]
+  public void Contains_returns_true()
+  {
+    const string word1 = "edcba";
+    const string word2 = "edcbaedcba";
+    
+    var words = new WordList { word1, word2 };
+
+    words.Contains(word1).ShouldBeTrue();
+    words.Contains(word2).ShouldBeTrue();
+  }
+
+  [Test]
+  public void GetCandidates_contains_returns_expected()
+  {
+    const string word1 = "edcba";
+    const string word2 = "ba";
+    const string word3 = "dcb";
+    const string anagram = "edcbaedcba";
+    
+    var words = new WordList { word1, word2, word3 };
+
+    words.GetCandidates(anagram).Contains(word1).ShouldBeTrue();
+    words.GetCandidates(anagram).Contains(word2).ShouldBeTrue();
+    words.GetCandidates(anagram).Contains(word3).ShouldBeTrue();
+  }
+
+  [Test]
+  public void GetCandidates_not_contains_returns_expected()
+  {
+    const string word1 = "edcba";
+    const string word2 = "ba";
+    const string word3 = "dcb";
+    const string anagram = "zxvnmjkl";
+    
+    var words = new WordList { word1, word2, word3 };
+    var candidates = words.GetCandidates(anagram);
+
+    candidates.Contains(word1).ShouldBeFalse();
+    candidates.Contains(word2).ShouldBeFalse();
+    candidates.Contains(word3).ShouldBeFalse();
+  }
+
+  [Test]
+  public void GetCandidates_short_anagram_returns_empty()
+  {
+    const string word1 = "edcba";
+    const string word2 = "ba";
+    const string word3 = "dcb";
+    const string anagram = "a";
+    
+    var words = new WordList { word1, word2, word3 };
+    var candidates = words.GetCandidates(anagram);
+
+    candidates.ShouldBeEmpty();
+  }
 }
