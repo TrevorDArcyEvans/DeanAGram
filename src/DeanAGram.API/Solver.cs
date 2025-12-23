3ed998ea725254363bb78d1a3691bef3a28dd9a2
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2025-12-23 14:09:40 +0000

    - recursively get all solutions

--- src/DeanAGram.API/Solver.cs
+++ src/DeanAGram.API/Solver.cs
@@ -4,8 +4,56 @@ public sealed class Solver(WordList _wordList)
 {
   public IEnumerable<IEnumerable<string>> GetSolutions(string anagram)
   {
+    var retval = new List<List<string>>();
     var candidates = _wordList.GetCandidates(anagram);
 
-    return [candidates];
+    foreach (var candidate in candidates)
+    {
+      ProcessCandidate(retval, new(), candidate, anagram);
+    }
+
+    // dedupe results
+    foreach (var soln in retval)
+    {
+      soln.Sort();
+    }
+
+    var dupeList = new List<List<string>>();
+    for (var i = 0; i < retval.Count - 1; i++)
+    {
+      for (var j = i + 1; j < retval.Count; j++)
+      {
+        if (retval[i].SequenceEqual(retval[j]))
+        {
+          dupeList.Add(retval[j]);
+        }
+      }
+    }
+
+    foreach (var dupe in dupeList)
+    {
+      retval.Remove(dupe);
+    }
+
+    return retval;
+  }
+
+  private void ProcessCandidate(List<List<string>> retval, List<string> thisSoln, string candidate, string anagram)
+  {
+    thisSoln.Add(candidate);
+    var remainder = anagram.GetRemainder(candidate);
+    if (string.IsNullOrEmpty(remainder))
+    {
+      // last word is wholly in anagram
+      retval.Add(thisSoln);
+      return;
+    }
+
+    var candidates = _wordList.GetCandidates(remainder);
+    foreach (var otherCandidate in candidates)
+    {
+      var otherSoln = new List<string>(thisSoln);
+      ProcessCandidate(retval, otherSoln, otherCandidate, remainder);
+    }
   }
 }
--- src/DeanAGram.Tests/Solver_Tests.cs
+++ src/DeanAGram.Tests/Solver_Tests.cs
@@ -47,4 +47,43 @@ public sealed class Solver_Tests
     res.ShouldContain("lake");
     res.ShouldContain("rake");
   }
+
+  [Test]
+  public void GetSolutions_03_multiple_succeeds()
+  {
+    var wordList = new WordList
+    {
+      "I".ToLowerInvariant(),
+      "am".ToLowerInvariant(),
+      "Lord".ToLowerInvariant(),
+      "Lot".ToLowerInvariant(),
+      "Voldemort".ToLowerInvariant(),
+      "mail".ToLowerInvariant(),
+      "to".ToLowerInvariant(),
+      "a".ToLowerInvariant(),
+      "or".ToLowerInvariant()
+    };
+    var sut = new Solver(wordList);
+
+    var result = sut.GetSolutions("Tom Marvolo Lot Riddle".ToLowerInvariant().RemoveWhitespace());
+
+    result.Count().ShouldBe(2);
+
+    //  "I am Lord Lot Voldemort"
+    var res1 = result.Single(x =>
+      x.Contains("I".ToLowerInvariant()) &&
+      x.Contains("am".ToLowerInvariant()) &&
+      x.Contains("Lord".ToLowerInvariant()) &&
+      x.Contains("Lot".ToLowerInvariant()) &&
+      x.Contains("Voldemort".ToLowerInvariant()));
+    res1.Count().ShouldBe(5);
+
+    //  "mail to Lord Voldemort"
+    var res2 = result.Single(x =>
+      x.Contains("mail".ToLowerInvariant()) &&
+      x.Contains("to".ToLowerInvariant()) &&
+      x.Contains("Lord".ToLowerInvariant()) &&
+      x.Contains("Voldemort".ToLowerInvariant()));
+    res2.Count().ShouldBe(4);
+  }
 }
